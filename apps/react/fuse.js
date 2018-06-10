b3a25e49de35059b5682d34e9aefe39f965d7b7b
assly <blue20will@gmail.com>
2018-06-10 14:53:49 +0900

    Delete requiring 'CSS, SCSSPlugin'

--- apps/react/fuse.js
+++ apps/react/fuse.js
@@ -1,14 +1,13 @@
 const path = require('path');
 const publicPath = path.resolve(__dirname, '..', '..', 'public');
 
-const { FuseBox, CSSPlugin, SassPlugin } = require('fuse-box');
+const { FuseBox } = require('fuse-box');
 const fuse = FuseBox.init({
   homeDir : 'frontend',
   target  : 'browser@es6',
   output  : `${publicPath}/$name.js`,
   useTypescriptCompiler: true,
   plugins : [
-    [SassPlugin(), CSSPlugin()]
   ],
   shim : {
   }
