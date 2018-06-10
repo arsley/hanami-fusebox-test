4a0c3d44c13756932e1b22fca95d7b4d9cf57380
assly <blue20will@gmail.com>
2018-06-10 15:03:31 +0900

    Setup for Babel

--- apps/react/fuse.js
+++ apps/react/fuse.js
@@ -1,16 +1,22 @@
 const path = require('path');
 const publicPath = path.resolve(__dirname, '..', '..', 'public');
 
-const { FuseBox } = require('fuse-box');
+const { FuseBox, BabelPlugin } = require('fuse-box');
 const fuse = FuseBox.init({
   homeDir : 'frontend',
   target  : 'browser@es6',
   output  : `${publicPath}/$name.js`,
   useTypescriptCompiler: true,
   plugins : [
+    BabelPlugin({
+      presets : ["es2015"],
+      plugins : [
+        ["transform-react-jsx"]
+      ]
+    })
   ],
   shim : {
   }
 });
-fuse.bundle('react-app').instructions('> application.js').watch('frontend/**');
+fuse.bundle('react-app').instructions('> application.jsx').watch('frontend/**');
 fuse.run();
